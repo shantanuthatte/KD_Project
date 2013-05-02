0c0e23c00b9339e0cfe90d57f2e61710ff05bc9c
Shantanu Thatte <shantanuthatte@gmail.com>
2013-05-01 22:28:56 -0500

    polishing of word filter
    added new way to parse sequentially paras, and headings

--- KD_Project/KD_Project/Form1.cs
+++ KD_Project/KD_Project/Form1.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Web;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using HtmlAgilityPack;
@@ -63,8 +64,8 @@ namespace KD_Project
 
         private static bool commonWordTest(string s)
         {
-            string common = "the be and of a in to have to it I that for you he with on do say this they at but we his from that not n't n't by she or as what go their can who get if would her all my make about know will as up one time there year so think when which them some me people take out into just see him your come could now than like other how then its our two more these want way look first also new because day more use no man find here thing give many well only those tell one very her even back any good woman through us life child there work down may after should call world over school still try in as last ask need too feel three when state never become between high really something most another much family own out leave put old while mean on keep student why let great same big group begin seem country help talk where turn problem every start hand might American show part about against place over such again few case most week company where system each right program hear so question during work play government run small number off always move like night live Mr point believe hold today bring happen next without before large all million must home under water room write mother area national money story young fact month different lot right study book eye job word though business issue side kind four head far black long both little house yes after since long provide service around friend important father sit away until power hour game often yet line political end among ever stand bad lose however member pay law meet car city almost include continue set later community much name five once white least president learn real change team minute best several idea kid body information nothing ago right lead social understand whether back watch together follow around parent only stop face anything create public already speak others read level allow add office spend door health person art sure such war history party within grow result open change morning walk reason low win research girl guy early food before moment himself air teacher force offer enough both education across although remember foot second boy maybe toward able age off policy everything love process music including consider appear actually buy probably human wait serve market die send expect home sense build stay fall oh nation plan cut college interest death course someone experience behind reach local kill six remain effect use yeah suggest class control raise care perhaps little late hard field else pass former sell major sometimes require along development themselves report role better economic effort up decide rate strong possible heart drug show leader light voice wife whole police mind finally pull return free military price report less according decision explain son hope even develop view relationship carry town road drive arm true federal break better difference thank receive";
-            HashSet<string> hash = new HashSet<string>(common.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries));
+            string common = "the be and of a in to have to it i that for you he with on do say this they at but we his from that not n't n't by she or as what go their can who get if would her all my make about know will as up one time there year so think when which them some me people take out into just see him your come could now than like other how then its our two more these want way look first also new because day more use no man find here thing give many well only those tell one very her even back any good woman through us life child there work down may after should call world over school still try in as last ask need too feel three when state never become between high really something most another much family own out leave put old while mean on keep student why let great same big group begin seem country help talk where turn problem every start hand might American show part about against place over such again few case most week company where system each right program hear so question during work play government run small number off always move like night live Mr point believe hold today bring happen next without before large all million must home under water room write mother area national money story young fact month different lot right study book eye job word though business issue side kind four head far black long both little house yes after since long provide service around friend important father sit away until power hour game often yet line political end among ever stand bad lose however member pay law meet car city almost include continue set later community much name five once white least president learn real change team minute best several idea kid body information nothing ago right lead social understand whether back watch together follow around parent only stop face anything create public already speak others read level allow add office spend door health person art sure such war history party within grow result open change morning walk reason low win research girl guy early food before moment himself air teacher force offer enough both education across although remember foot second boy maybe toward able age off policy everything love process music including consider appear actually buy probably human wait serve market die send expect home sense build stay fall oh nation plan cut college interest death course someone experience behind reach local kill six remain effect use yeah suggest class control raise care perhaps little late hard field else pass former sell major sometimes require along development themselves report role better economic effort up decide rate strong possible heart drug show leader light voice wife whole police mind finally pull return free military price report less according decision explain son hope even develop view relationship carry town road drive arm true federal break better difference thank receive";
+            HashSet<string> hash = new HashSet<string>(common.ToLower().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries));
             return(hash.Contains(s));
         }
         /*
@@ -94,11 +95,11 @@ namespace KD_Project
         {
             //Console.WriteLine(e.Result);
             HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
-            doc.LoadHtml(e.Result);
+            doc.LoadHtml(HttpUtility.HtmlDecode(e.Result));
 
             //Rekha
             //var nodes = doc.DocumentNode.SelectNodes("//script|//style|//img|//table|//dl|//sup|//pre|//div[contains(concat(' ',@class,' '),' thumb ')]|//div[@class='dablink']|//div[@id='siteSub']|//div[@id='jump-to-nav']|//div[contains(concat(' ',@class,' '),' metadata ')]|//div[contains(concat(' ',@class,' '),' rellink ')]|//ol[@class='references']|//div[contains(concat(' ',@class,' '),' refbegin ')]|//div[@class='printfooter']|//div[@id='catlinks']|//div[@id='mw-navigation']|//div[@id='footer']");
-            var nodes = doc.DocumentNode.SelectNodes("//h1|//h2|//h3|//h4|//h5|//h6|//script|//style|//img|//table|//dl|//sup|//pre|//div[contains(concat(' ',@class,' '),' thumb ')]|//div[@class='dablink']|//div[@id='siteSub']|//div[@id='jump-to-nav']|//div[contains(concat(' ',@class,' '),' metadata ')]|//div[contains(concat(' ',@class,' '),' rellink ')]|//ol[@class='references']|//div[contains(concat(' ',@class,' '),' refbegin ')]|//div[@class='printfooter']|//div[@id='catlinks']|//div[@id='mw-navigation']|//div[@id='footer']");
+            var nodes = doc.DocumentNode.SelectNodes("//script|//style|//img|//table|//dl|//sup|//pre|//div[contains(concat(' ',@class,' '),' thumb ')]|//div[@class='dablink']|//div[@id='siteSub']|//div[@id='jump-to-nav']|//div[contains(concat(' ',@class,' '),' metadata ')]|//div[contains(concat(' ',@class,' '),' rellink ')]|//ol[@class='references']|//div[contains(concat(' ',@class,' '),' refbegin ')]|//div[@class='printfooter']|//div[@id='catlinks']|//div[@id='mw-navigation']|//div[@id='footer']");
 
             foreach (var node in nodes)
                 node.ParentNode.RemoveChild(node);
@@ -110,7 +111,10 @@ namespace KD_Project
            var s = doc.DocumentNode.SelectSingleNode("//div[@id='mw-content-text']");
             //var s = doc.DocumentNode.SelectSingleNode("//h[1-9]");
 
-
+            var ns = s.SelectNodes("h1|h2|h3|h4|h5|h6|p");
+            foreach (HtmlNode n in ns){
+                Console.WriteLine(n.Name);
+            }
             string selText = s.InnerText;
             /*
             HtmlNode someNode = doc.GetElementbyId("mw-content-text");
@@ -137,8 +141,9 @@ namespace KD_Project
             List<string> hrefTags = new List<string>();
             List<string> finalhrefTags = new List<string>();
             int countLinks = 0;
-            var baseUrl = new Uri("http://en.wikipedia.org/w/index.php?title="+textBox1.Text+"&printable=yes");
-            foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
+            var baseUrl = new Uri("http://en.wikipedia.org/w/index.php?title="+ HttpUtility.UrlEncode(textBox1.Text) +"&printable=yes");
+            ns = s.SelectNodes("p/a");
+            foreach (HtmlNode link in ns)
             {
                 HtmlAttribute att = link.Attributes["href"];
                 hrefTags.Add(att.Value);
@@ -184,7 +189,16 @@ namespace KD_Project
             ILemmatizer lemm = new LemmatizerPrebuiltCompact(LanguagePrebuilt.English);
             foreach (string word in swords)
             {
-                lwords.Add(lemm.Lemmatize(word.ToLower()));
+                if (word.Length == 1)
+                    continue;
+                if (word.Length <= 3)
+                {
+                    //Console.WriteLine(word);
+                    lwords.Add(word.ToLower());
+                }
+                else
+                    lwords.Add(lemm.Lemmatize(word));
+
             }
             List<string> fwords = new List<string>();
             fwords = lwords.Where(x => !commonWordTest(x)).ToList();
